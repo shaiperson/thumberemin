60077db44ab3de641d65014c49042cb5440f4478
Shai <shai.bianchi@mercadolibre.com>
2017-01-27 20:09:25 -0300

    Code cleanup

--- src/config.cpp
+++ src/config.cpp
@@ -1,30 +1,5 @@
 #include "../include/config.h"
 
-// int config::defaultCamera = 0;
-// string config::gameWindowName = "Theremin";
-// Size config::frameSize;
-//
-// Rect config::playingRegion = Rect ( // TODO hardcoded
-//     config::frameSize.width*(10/12.0),
-//     0,
-//     config::frameSize.width/12.0,
-//     config::frameSize.height
-// );
-//
-// vector<Rect> config::inactiveRegions = {
-//     Rect(0, 0, config::playingRegion.x, config::frameSize.height),
-//     Rect(config::playingRegion.x+config::playingRegion.width, 0, config::frameSize.width-config::playingRegion.x-config::playingRegion.width-1, config::frameSize.height),
-// };
-//
-// Rect config::samplingRegion = Rect (
-//     config::playingRegion.x + 0.25*config::playingRegion.width,
-//     config::playingRegion.height*(10/12.0),
-//     config::playingRegion.width*0.5,
-//     config::playingRegion.width*0.5
-// );
-//
-// Size config::trackingWindowSize = samplingRegion.size();
-
 DynamicConfiguration::DynamicConfiguration(const Size& fsz) {
     frameSize = Size(fsz);
     playingRegion = Rect ( // TODO hardcoded
