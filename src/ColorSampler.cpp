08728423120bb067f945b2c94472ae69412b5064
Shai <shai.bianchi@mercadolibre.com>
2017-01-28 02:12:08 -0300

    Use ROI instead of masking for color sampling

--- src/ColorSampler.cpp
+++ src/ColorSampler.cpp
@@ -9,28 +9,8 @@ Point ColorSampler::current() const {
 }
 
 Mat ColorSampler::takeSample(Mat& frame) {
-    // Mat resultB;
-    // Mat resultG;
-    // Mat resultR;
-    //
-    // vector<Mat> bgrPlanes;
-    // split(frame, bgrPlanes);
-    // int histSize = 256;
-    // float range[] = {0, 256}; // lower boundary inclusive, upper boundary exclusive
-    // const float* histRange = {range};
-    //
-    // Mat samplingRegionMask = zeros(dynconf.frameSize, frame.type());
-    // Mat maskSubregion = samplingRegionMask(dynconf.samplingRegion);
-    // maskSubregion.setTo(1);
-    //
-    // calcHist( &bgrPlanes[0], 1, 0, samplingRegionMask, resultB, 1, &histSize, &histRange);
-    // calcHist( &bgrPlanes[1], 1, 0, samplingRegionMask, resultG, 1, &histSize, &histRange);
-    // calcHist( &bgrPlanes[2], 1, 0, samplingRegionMask, resultR, 1, &histSize, &histRange);
-
     /* INTENTO DE HISTOGRAMA 3D */
-    Mat samplingRegionMask = Mat::zeros(dynconf.frameSize, CV_8UC1);
-    Mat maskRoi = samplingRegionMask(dynconf.samplingRegion);
-    maskRoi.setTo(1); // sets ROI values to 1 in samplingRegionMask
+    Mat roi = frame(dynconf.samplingRegion);
 
     int nimages = 1; // only one 3-channel image
     const int channels[3] = {0,1,2};
@@ -40,7 +20,7 @@ Mat ColorSampler::takeSample(Mat& frame) {
     const float* ranges[3] = {singleBinRange, singleBinRange, singleBinRange};
 
     Mat hist;
-    calcHist(&frame, nimages, channels, samplingRegionMask, hist, dims, histSize, ranges);
+    calcHist(&roi, nimages, channels, Mat(), hist, dims, histSize, ranges);
 
     return hist;
 }
