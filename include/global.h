cd8abf6d8f36a0ee753671f033046e7a235a7641
Shai <shai.bianchi@mercadolibre.com>
2017-01-29 18:35:19 -0300

    Edit tracking marker. Add text indicating pixel row index

--- include/global.h
+++ include/global.h
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <exception>
 #include <vector>
+#include <string>
 
 using namespace cv;
 using namespace std;
--- src/PlayingScreen.cpp
+++ src/PlayingScreen.cpp
@@ -9,7 +9,30 @@ void PlayingScreen::processFrame(Mat& frame, const TrackingInfo& tracker) const 
 }
 
 void PlayingScreen::drawNoteMarker(Mat& frame, const Point& center) const {
+    /* "FREQUENCY POINTER" */
     circle (
+        frame,
+        center,
+        dynconf.trackingMarkerRadius * (1/48.0),
+        StaticConfiguration::trackingMarkerColor,
+        10,
+        CV_AA
+    );
+
+    Point freqPoint(center - Point(dynconf.trackingMarkerRadius * 2, 0));
+    arrowedLine (
+        frame,
+        center,
+        freqPoint,
+        StaticConfiguration::trackingMarkerColor,
+        1,
+        CV_AA
+    );
+
+    putText(frame, to_string(freqPoint.y), freqPoint - Point(0, 5), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(255,255,255));
+
+    /* ENCLOSING CIRCLES */
+    circle ( // inner
         frame,
         center,
         dynconf.trackingMarkerRadius * (2/3.0),
@@ -18,24 +41,7 @@ void PlayingScreen::drawNoteMarker(Mat& frame, const Point& center) const {
         CV_AA
     );
 
-    line (
-        frame,
-        center,
-        center-Point(dynconf.trackingMarkerRadius * 2, 0),
-        StaticConfiguration::trackingMarkerColor,
-        1,
-        CV_AA
-    );
-
-    circle (
-        frame,
-        center,
-        dynconf.trackingMarkerRadius * (1/12.0),
-        StaticConfiguration::trackingMarkerColor,
-        10,
-        CV_AA
-    );
-    circle (
+    circle ( // outer
         frame,
         center,
         dynconf.trackingMarkerRadius,
