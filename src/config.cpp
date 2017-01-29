020d2f1e3604e8ec913825dc940e35c763900e2a
Shai <shai.bianchi@mercadolibre.com>
2017-01-29 13:46:10 -0300

    Add comments

--- src/config.cpp
+++ src/config.cpp
@@ -7,23 +7,32 @@ const Scalar StaticConfiguation::trackingWindowColor = Scalar(255,191,0);
 const int StaticConfiguation::trackingWindowThickness = 2;
 
 DynamicConfiguration::DynamicConfiguration(const Size& fsz) {
+    /* Size of the frames fed by the camera in use */
     frameSize = Size(fsz);
+
+    /* Denotes the region of the screen where motion tracking happens */
     playingRegion = Rect ( // TODO hardcoded
         frameSize.width*(41/48.0),
         0,
         frameSize.width*(2/48.0),
         frameSize.height
     );
+
+    /* A collection of rectangles denoting the rest of the screen */
     inactiveRegions = {
         Rect(0, 0, playingRegion.x, frameSize.height),
         Rect(playingRegion.x+playingRegion.width, 0, frameSize.width-playingRegion.x-playingRegion.width-1, frameSize.height),
     };
+
+    /* Denotes the subregion of the playing region from where the color sample is taken for tracking */
     samplingRegion = Rect (
         playingRegion.x,
         playingRegion.height*(40/48.0),
         playingRegion.width,
         playingRegion.width
     );
+
+    /* Size of the sampling region */
     trackingWindowSize = samplingRegion.size();
 }
 
