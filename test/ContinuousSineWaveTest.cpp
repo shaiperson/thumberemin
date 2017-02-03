c1c1c9b7475286e7a68dad9b7db4808e4191a6b7
Shai <shai.bianchi@mercadolibre.com>
2017-02-03 00:12:27 -0300

    Add cycle size test for continuous wave generator

--- test/ContinuousSineWaveTest.cpp
+++ test/ContinuousSineWaveTest.cpp
@@ -3,10 +3,30 @@
 #include "../include/ContinuousSineWave.h"
 #include "../include/global.h"
 
-TEST_CASE("Cycle size") {
-    ContinuousSineWave csw(1, 1);
+SCENARIO ("Generating continuous wave with certain frequency and sample rate") {
+    ContinuousSineWave csw(1, 100);
 
-    SECTION("First cycle size is correct with respect to sample rate") {
-        // REQUIRE(csw.cycleSize() == );
+    SECTION ("Cycle size returned by method") {
+
+        SECTION ("Before updating frequency") {
+            REQUIRE(csw.cycleSize() == 100);
+        }
+
+        SECTION ("After updating frequency") {
+            csw.updateFrequency(2);
+            REQUIRE(csw.cycleSize() == 50);
+        }
+    }
+
+    SECTION ("Cycle size as observed by calculating next cycle samples") {
+
+        SECTION ("Before updating frequency") {
+            REQUIRE(csw.nextCycle().size() == 100);
+        }
+
+        SECTION ("After updating frequency") {
+            csw.updateFrequency(2);
+            REQUIRE(csw.cycleSize() == 50);
+        }
     }
 }
