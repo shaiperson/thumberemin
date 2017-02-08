ff11c96d3dfa8dde3358d460f009b3b7ab55d654
Shai <shai.bianchi@mercadolibre.com>
2017-02-08 14:30:55 -0300

    Use sparse matrix for testing histogram

--- test/ihtTests.cpp
+++ test/ihtTests.cpp
@@ -11,13 +11,14 @@ SCENARIO("Calculating the 3D histogram of an RGB 8-bit image") {
         WHEN("Histogram is calculated") {
             IHT_calc3DByteDepthUniformHist(&image, &hist);
 
-            // THEN("Histogram has 25, total number of image pixels, in [1,2,3] bin") {
-            //     REQUIRE(hist.ref<float>(1,2,3) == 25); // float comparison by == is OK since these are simply counters
-            // }
-            //
-            // THEN("And it has 0 in all other bins") {
-            //     REQUIRE(hist.nzcount() == 1);
-            // }
+            THEN("Histogram has 25, total number of image pixels, in [1,2,3] bin") {
+                REQUIRE(hist.at<float>(1,2,3) == 25); // float comparison by == is OK since these are simply counters
+            }
+
+            THEN("And it has 0 in all other bins") {
+                SparseMat sparseHist(hist);
+                REQUIRE(sparseHist.nzcount() == 1);
+            }
         }
     }
 }
