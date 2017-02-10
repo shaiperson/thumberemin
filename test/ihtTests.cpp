055d5881b04bd88e7c996242c2b995fd4ab42380
Shai <shai.bianchi@mercadolibre.com>
2017-02-09 21:32:36 -0300

    Use non-continuous mock image for histogram test

--- test/ihtTests.cpp
+++ test/ihtTests.cpp
@@ -3,20 +3,22 @@
 #include "catch.hpp"
 #include "../include/in-house-tracking/iht.h"
 
-SCENARIO("Calculating the 3D histogram of an RGB 8-bit image") {
+SCENARIO("Calculating the 3D histogram of an RGB 8-bit image", "[hist]") {
     GIVEN("A 5x5 RGB 8-bit image whose pixels are all [1,2,3]") {
-        Mat image(5, 5, CV_8UC3, Scalar(1,2,3));
+        Mat imageToCropFrom(6, 6, CV_8UC3, Scalar(1,2,3));
+        Mat image = imageToCropFrom(Rect(1,1,5,5)); // crop image so that it is not continuous
+
         Mat hist = IHT_createHistArgument();
 
         WHEN("Histogram is calculated") {
             IHT_calc3DByteDepthUniformHist(&image, &hist);
 
-            THEN("Histogram has 25, total number of image pixels, in [1,2,3] bin") {
+            THEN("Histogram at [1,2,3] has 25, total number of image pixels") {
                 REQUIRE(hist.at<float>(1,2,3) == 25); // float comparison by == is OK since these are simply counters
             }
 
             THEN("And it has 0 in all other bins") {
-                SparseMat sparseHist(hist);
+                SparseMat sparseHist(hist); // use a sparse matrix for testing to avoid iterating through dense-matrix histogram of size 256^3 = 2^24
                 REQUIRE(sparseHist.nzcount() == 1);
             }
         }
