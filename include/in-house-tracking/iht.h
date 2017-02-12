da172e7b4ad4993388c5167e3a352e62499244cf
Shai <bianchishai@gmail.com>
2017-02-12 13:18:18 -0300

    Use C-linkage for IHT functions

--- include/in-house-tracking/iht.h
+++ include/in-house-tracking/iht.h
@@ -7,8 +7,8 @@
 void IHT_calc3DByteDepthUniformHist(const Mat* image, Mat* hist);
 void IHT_calc3DByteDepthBackProject(const Mat* image, const Mat* hist, Mat* result);
 
-extern void IHT_calc3DByteDepthUniformHist_ASM(const Mat* image, Mat* hist);
-extern void IHT_calc3DByteDepthBackProject_ASM(const Mat* image, const Mat* hist, Mat* result);
+extern "C" int IHT_calc3DByteDepthUniformHist_ASM(const Mat* image, Mat* hist);
+extern "C" void IHT_calc3DByteDepthBackProject_ASM(const Mat* image, const Mat* hist, Mat* result);
 
 /* timer wrappers */
 void IHT_startTimer();
