ecd50b2336eca699a151e12be94e9c58c2be5591
Shai <bianchishai@gmail.com>
2017-02-13 16:58:03 -0300

    Add time measuring tests

--- test/times.cpp
+++ test/times.cpp
@@ -1,9 +1,18 @@
 #include "catch.hpp"
 #include "../include/in-house-tracking/iht.h"
 
-TEST_CASE("Histogram calculation, sequential", "[sequential],[histogram]") {
-    Mat image = imread("ihtinput/rgbseq1.png");
+TEST_CASE("Histogram calculation, sequential", "[times],[sequential],[histogram]") {
+    Mat image = imread("../test/ihtinput/rgballover.png");
     Mat hist = IHT_createHistArgumentShort();
-    IHT_calc3DByteDepthUniformHist(&image, &hist);
-    cout << "Time: " << timer::t << endl;
+
+    IHT_calc3DByteDepthUniformHist(image.data, hist.data, image.rows, image.cols, image.step);
+    cerr << timer::t << endl;
+}
+
+TEST_CASE("Histogram calculation, vectorial", "[times],[vectorial],[histogram]") {
+    Mat image = imread("../test/ihtinput/rgballover.png");
+    Mat hist = IHT_createHistArgumentShort();
+
+    IHT_calc3DByteDepthUniformHist_ASM(image.data, hist.data, image.rows, image.cols, image.step);
+    cerr << timer::t << endl;
 }
