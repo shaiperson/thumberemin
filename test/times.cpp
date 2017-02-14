6776f67e02e4eca9b66e642e3bd8ae5adb7639fe
Shai <bianchishai@gmail.com>
2017-02-13 23:21:12 -0300

    Refactor times test

--- test/times.cpp
+++ test/times.cpp
@@ -1,18 +1,37 @@
 #include "catch.hpp"
 #include "../include/in-house-tracking/iht.h"
 
-TEST_CASE("Histogram calculation, sequential", "[times],[sequential],[histogram]") {
-    Mat image = imread("../test/ihtinput/rgballover.png");
-    Mat hist = IHT_createHistArgumentShort();
+#define REPETITIONS 300
 
-    IHT_calc3DByteDepthUniformHist(image.data, hist.data, image.rows, image.cols, image.step);
-    cerr << timer::t << endl;
-}
+TEST_CASE("Histogram calculation times", "[times],[sequential],[histogram]") {
+    Mat image, hist;
+    double time_its, time_ptrs, time_asm = 0;
 
-TEST_CASE("Histogram calculation, vectorial", "[times],[vectorial],[histogram]") {
-    Mat image = imread("../test/ihtinput/rgballover.png");
-    Mat hist = IHT_createHistArgumentShort();
+    image = imread("../test/ihtinput/rgballover.png");
 
-    IHT_calc3DByteDepthUniformHist_ASM(image.data, hist.data, image.rows, image.cols, image.step);
-    cerr << timer::t << endl;
+    for (size_t i = 0; i < REPETITIONS; ++i) {
+        /* measure iterators version */
+        hist = IHT_createHistArgumentShort();
+        IHT_calc3DByteDepthUniformHist_CV(image, hist);
+        time_its += timer::t;
+
+        /* measure pointers version */
+        hist = IHT_createHistArgumentShort();
+        IHT_calc3DByteDepthUniformHist(image.data, hist.data, image.rows, image.cols, image.step);
+        time_ptrs += timer::t;
+
+        /* measure assembler version */
+        hist = IHT_createHistArgumentShort();
+        IHT_calc3DByteDepthUniformHist_ASM(image.data, hist.data, image.rows, image.cols, image.step);
+        time_asm += timer::t;
+    }
+
+    cout << "C++ iterators ";
+    cerr << time_its / REPETITIONS << endl;
+
+    cout << "C++ pointers  ";
+    cerr << time_ptrs / REPETITIONS << endl;
+
+    cout << "Assembler SSE ";
+    cerr << time_asm / REPETITIONS << endl;
 }
