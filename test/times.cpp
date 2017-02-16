fab37cc8f44afa5ef592b11dead13f8c1b66dbd8
Shai <bianchishai@gmail.com>
2017-02-16 03:04:59 -0300

    Add time tests for back projection

--- test/times.cpp
+++ test/times.cpp
@@ -3,7 +3,7 @@
 
 #define REPETITIONS 300
 
-TEST_CASE("Histogram calculation times", "[times],[sequential],[histogram]") {
+TEST_CASE("Histogram calculation times", "[times],[histogram]") {
     Mat image, hist;
     double time_its, time_ptrs, time_asm, time_disasm = 0;
 
@@ -42,5 +42,33 @@ TEST_CASE("Histogram calculation times", "[times],[sequential],[histogram]") {
 
     cout << "Disassembled O3 ";
     cerr << time_disasm / REPETITIONS << endl;
+
+}
+
+TEST_CASE("Back projection calculation times", "[times],[backproject]") {
+    Mat image, hist, backproject;
+    double time_ptrs, time_asm = 0;
+
+    image = imread("../test/ihtinput/gen/big_alternate_b_w.png");
+
+    for (size_t i = 0; i < REPETITIONS; ++i) {
+        /* measure iterators version */
+        hist = IHT_createHistArgumentShort();
+        backproject = IHT_createBackProjectArgumentShort(image.size());
+        IHT_calc3DByteDepthBackProject(image.data, hist.data, backproject.data, image.rows, image.cols, image.step);
+        time_ptrs += timer::t;
+
+        /* measure pointers version */
+        hist = IHT_createHistArgumentShort();
+        backproject = IHT_createBackProjectArgumentShort(image.size());
+        IHT_calc3DByteDepthBackProject_ASM(image.data, hist.data, backproject.data, image.rows, image.cols, image.step);
+        time_asm += timer::t;
+    }
+
+    cout << "C++ pointers    ";
+    cerr << time_ptrs / REPETITIONS << endl;
+
+    cout << "Assembler SSE   ";
+    cerr << time_asm / REPETITIONS << endl;
 
 }
