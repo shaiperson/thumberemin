806fabfbebde75b2c53044b2c81eb640ff5b7da1
Shai <bianchishai@gmail.com>
2017-02-21 02:13:53 -0300

    [mean shift ASM] Parameter handling

--- asm/meanshift.asm
+++ asm/meanshift.asm
@@ -7,10 +7,44 @@ section .data
 
 section .text
 
+; rdi->r12 <-- densityMap
+; esi->r13 <-- mapRows
+; edx->r14 <-- mapcols
+; ecx->r15 <-- mapstep
+; r8->rbx  <-- &window
+; r9       <-- iters
+
+; Quedarían conteniendo parámetros, en principio: r12, r13, r14, r15, rbx, r9(desp de volver a traerlo de la pila)
+; Quedarían libres entonces, en principio: r8, r10, r11, rdi, rsi, rdx, rcx
+
 IHT_meanShift_ASM:
+
+    push r12
+    push r13
+    push r14
+    push r15
+    push rbx
+
+    ; preserve parameters
+    mov r12, rdi
+    mov r13, rsi
+    mov r14, rdx
+    mov r15, rcx
+    mov rbx, r8
+
+    sub rsp, 8
+    mov [rsp], r9 ; preservo parámetro de r9, iters
 
     call GLOBAL_startTimer
 
+    mov r9, [rsp] ; vuelvo a traer de la pila iters
+
     call GLOBAL_stopTimer
-    
+
+    pop rbx
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+
     ret
