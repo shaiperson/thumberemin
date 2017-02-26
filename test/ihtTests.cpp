3d0b0eed98fb0e13ec3864f281c93df0cb9cc18d
Shai <bianchishai@gmail.com>
2017-02-25 21:31:41 -0300

    [IHT hist ASM] Add tests for images with number of cols not multiple of 5

--- test/ihtTests.cpp
+++ test/ihtTests.cpp
@@ -7,8 +7,8 @@ Mat image, hist_seq, hist_vec;
 
 SCENARIO("Calculating the 3D histogram of an RGB 8-bit image with cols divisble by 5", "[unit], [hist]") {
     GIVEN("A 10x10 RGB 8-bit image whose pixels are all [1,2,3]") {
-        Mat imageToCropFrom(13, 13, CV_8UC3, Scalar(1,2,3));
-        image = imageToCropFrom(Rect(2,2,10,10)); // crop image so that it has padding
+        Mat imageToCropFrom(16, 16, CV_8UC3, Scalar(1,2,3));
+        image = imageToCropFrom(Rect(2,2,13,13)); // crop image so that it has padding
         hist_seq = IHT_createHistArgumentShort();
         hist_vec = IHT_createHistArgumentShort();
 
@@ -16,9 +16,9 @@ SCENARIO("Calculating the 3D histogram of an RGB 8-bit image with cols divisble 
             IHT_calc3DByteDepthUniformHist(image.data, hist_seq.data, image.rows, image.cols, image.step);
             IHT_calc3DByteDepthUniformHist_ASM(image.data, hist_vec.data, image.rows, image.cols, image.step);
 
-            THEN("Histogram at [1,2,3] has 100, total number of image pixels") {
-                REQUIRE(hist_seq.at<short>(3,2,1) == 100);
-                REQUIRE(hist_vec.at<short>(3,2,1) == 100);
+            THEN("Histogram at [1,2,3] has 169, total number of image pixels") {
+                REQUIRE(hist_seq.at<short>(3,2,1) == 169);
+                REQUIRE(hist_vec.at<short>(3,2,1) == 169);
             }
 
             THEN("It has 0 in all other bins") {
@@ -30,9 +30,9 @@ SCENARIO("Calculating the 3D histogram of an RGB 8-bit image with cols divisble 
         }
     }
 
-    GIVEN("A 5x5 RGB 8-bit image with cols divisible by 5 whose pixels are (0, 0, 0..24)") {
-        Mat imageToCropFrom(6, 6, CV_8UC3);
-        image = imageToCropFrom(Rect(1,1,5,5)); // crop image so that it has padding
+    GIVEN("A 7x7 RGB 8-bit image with whose pixels are (0, 0, 0..48)") {
+        Mat imageToCropFrom(9, 9, CV_8UC3);
+        image = imageToCropFrom(Rect(1,1,7,7)); // crop image so that it has padding
         for (auto it = image.begin<Vec3b>(); it != image.end<Vec3b>(); ++it)
             *it = Vec3b(0, 0, it - image.begin<Vec3b>());
         hist_seq = IHT_createHistArgumentShort();
@@ -42,15 +42,15 @@ SCENARIO("Calculating the 3D histogram of an RGB 8-bit image with cols divisble 
             IHT_calc3DByteDepthUniformHist(image.data, hist_seq.data, image.rows, image.cols, image.step);
             IHT_calc3DByteDepthUniformHist_ASM(image.data, hist_vec.data, image.rows, image.cols, image.step);
 
-            THEN("Histogram has 1 in bins (0, 0, 0..24)") {
+            THEN("Histogram has 1 in bins (0, 0, 0..48)") {
                 bool allOnes_seq = true;
-                for (size_t i = 0; i < 25; ++i)
+                for (size_t i = 0; i < 49; ++i)
                     allOnes_seq = allOnes_seq && hist_seq.at<short>(i,0,0) == 1;
 
                 REQUIRE(allOnes_seq);
 
                 bool allOnes_vec = true;
-                for (size_t i = 0; i < 25; ++i)
+                for (size_t i = 0; i < 49; ++i)
                     allOnes_vec = allOnes_vec && hist_vec.at<short>(i,0,0) == 1;
 
                 REQUIRE(allOnes_vec);
@@ -59,8 +59,8 @@ SCENARIO("Calculating the 3D histogram of an RGB 8-bit image with cols divisble 
             THEN("It has 0 in all other bins") {
                 SparseMat sparseHist_seq(hist_seq); // use a sparse matrix for testing to avoid iterating through dense-matrix histogram of size 256^3 = 2^24
                 SparseMat sparseHist_vec(hist_vec);
-                REQUIRE(sparseHist_seq.nzcount() == 25);
-                REQUIRE(sparseHist_vec.nzcount() == 25);
+                REQUIRE(sparseHist_seq.nzcount() == 49);
+                REQUIRE(sparseHist_vec.nzcount() == 49);
             }
         }
     }
