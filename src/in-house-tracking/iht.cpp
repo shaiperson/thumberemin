9f15c140531e8f3c8dd053fb74d048b1539dcad0
Shai <bianchishai@gmail.com>
2017-02-26 21:13:08 -0300

    [IHT mean shift] Check for updated window TL corner for negative values before updating

--- src/in-house-tracking/iht.cpp
+++ src/in-house-tracking/iht.cpp
@@ -153,11 +153,13 @@ void IHT_meanShift (
         tl_y = roundf(m01/m00 + (float)curr_w_y - height*0.5);
 
         if ( // intersection between current and previous windows not empty
+            tl_x >= 0 &&
+            tl_y >= 0 &&
             abs(tl_x - curr_w_x) < width &&
             abs(tl_y - curr_w_y) < height
         ) {
-            curr_w_x = std::min(std::max(tl_x, 0), mapcols-width);
-            curr_w_y = std::min(std::max(tl_y, 0), maprows-height);
+            curr_w_x = std::min(tl_x, mapcols-width);
+            curr_w_y = std::min(tl_y, maprows-height);
         }
 
         iterCounter += 1;
@@ -184,10 +186,10 @@ void IHT_meanShift_CV(const Mat& densityMap, Rect& window, size_t iters) {
             roundf(ms.m01/ms.m00 + (float)window.tl().y - window.height*0.5)
         );
 
-        if ( ( window & Rect(tl, window.size()) ) != Rect() ) {
+        if ( tl.x >= 0 && tl.y >= 0 && ( window & Rect(tl, window.size()) ) != Rect() ) {
             window = Rect (
-                std::min(std::max(tl.x, 0), densityMap.cols - window.width),
-                std::min(std::max(tl.y, 0), densityMap.rows - window.height),
+                std::min(tl.x, densityMap.cols - window.width),
+                std::min(tl.y, densityMap.rows - window.height),
                 window.width,
                 window.height
             );
