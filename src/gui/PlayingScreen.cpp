8d2b0b7a785bdab376999bf1f449ddb03da04c95
Shai <bianchishai@gmail.com>
2017-03-01 22:36:00 -0300

    [Entregable] Change tracking marker to circle

--- src/gui/PlayingScreen.cpp
+++ src/gui/PlayingScreen.cpp
@@ -13,49 +13,9 @@ void PlayingScreen::drawTrackingMarker(Mat& frame, const Point& center) const {
     circle (
         frame,
         center,
-        dynconf.trackingMarkerRadius * (1/48.0),
+        dynconf.trackingMarkerRadius*0.25,
         StaticConfiguration::trackingMarkerColor,
         10,
-        CV_AA
-    );
-
-    /*
-    Point freqPoint(center - Point(dynconf.trackingMarkerRadius * 3, 0));
-    line (
-        frame,
-        center,
-        freqPoint,
-        StaticConfiguration::trackingMarkerColor,
-        1,
-        CV_AA
-    );
-    */
-    /* print frequency */
-    // float freq = dynconf.pixel2Freq[center.y];
-    // stringstream stream;
-    // stream << fixed << setprecision(1) << freq;
-
-    // putText(frame, stream.str(), freqPoint - Point(0, 5), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(255,255,255));
-
-    /* ENCLOSING CIRCLES */
-
-    // inner
-    circle (
-        frame,
-        center,
-        dynconf.trackingMarkerRadius * (2/3.0),
-        StaticConfiguration::trackingMarkerColor,
-        10,
-        CV_AA
-    );
-
-    // outer
-    circle (
-        frame,
-        center,
-        dynconf.trackingMarkerRadius,
-        StaticConfiguration::trackingMarkerColor,
-        StaticConfiguration::trackingMarkerThickness,
         CV_AA
     );
 }
